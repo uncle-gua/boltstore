ec98775746bb9230711359268ed3db78f799b8e3
uncle-gua <scnb2681@163.com>
2024-05-06 08:59:14 +0800

    initial commit

--- boltstore.go
+++ boltstore.go
@@ -15,10 +15,10 @@ const sessionIDLen = 32
 const defaultMaxAge = 60 * 60 * 24 * 30 // 30 days
 const defaultPath = "/"
 
-type Store struct {
-	store       *bolthold.Store
-	Codecs      []securecookie.Codec
-	SessionOpts *sessions.Options
+type BoltStore struct {
+	store   *bolthold.Store
+	Codecs  []securecookie.Codec
+	Options *sessions.Options
 }
 
 type Session struct {
@@ -30,11 +30,11 @@ type Session struct {
 	ExpiresAt time.Time `boltholdIndex:"ExpiresAt"`
 }
 
-func New(store *bolthold.Store, keyPairs ...[]byte) *Store {
-	st := &Store{
+func New(store *bolthold.Store, keyPairs ...[]byte) *BoltStore {
+	st := &BoltStore{
 		store:  store,
 		Codecs: securecookie.CodecsFromPairs(keyPairs...),
-		SessionOpts: &sessions.Options{
+		Options: &sessions.Options{
 			Path:   defaultPath,
 			MaxAge: defaultMaxAge,
 		},
@@ -43,17 +43,17 @@ func New(store *bolthold.Store, keyPairs ...[]byte) *Store {
 	return st
 }
 
-func (st *Store) Get(r *http.Request, name string) (*sessions.Session, error) {
+func (st *BoltStore) Get(r *http.Request, name string) (*sessions.Session, error) {
 	return sessions.GetRegistry(r).Get(st, name)
 }
 
-func (st *Store) New(r *http.Request, name string) (*sessions.Session, error) {
+func (st *BoltStore) New(r *http.Request, name string) (*sessions.Session, error) {
 	session := sessions.NewSession(st, name)
-	opts := *st.SessionOpts
+	opts := *st.Options
 	session.Options = &opts
 	session.IsNew = true
 
-	st.MaxAge(st.SessionOpts.MaxAge)
+	st.MaxAge(st.Options.MaxAge)
 
 	s := st.getSessionFromCookie(r, session.Name())
 	if s != nil {
@@ -67,7 +67,7 @@ func (st *Store) New(r *http.Request, name string) (*sessions.Session, error) {
 	return session, nil
 }
 
-func (st *Store) Save(r *http.Request, w http.ResponseWriter, session *sessions.Session) error {
+func (st *BoltStore) Save(r *http.Request, w http.ResponseWriter, session *sessions.Session) error {
 	s := st.getSessionFromCookie(r, session.Name())
 
 	if session.Options.MaxAge < 0 {
@@ -119,7 +119,7 @@ func (st *Store) Save(r *http.Request, w http.ResponseWriter, session *sessions.
 	return nil
 }
 
-func (st *Store) getSessionFromCookie(r *http.Request, name string) *Session {
+func (st *BoltStore) getSessionFromCookie(r *http.Request, name string) *Session {
 	if cookie, err := r.Cookie(name); err == nil {
 		sessionID := ""
 		if err := securecookie.DecodeMulti(name, cookie.Value, &sessionID, st.Codecs...); err != nil {
@@ -135,8 +135,8 @@ func (st *Store) getSessionFromCookie(r *http.Request, name string) *Session {
 	return nil
 }
 
-func (st *Store) MaxAge(age int) {
-	st.SessionOpts.MaxAge = age
+func (st *BoltStore) MaxAge(age int) {
+	st.Options.MaxAge = age
 	for _, codec := range st.Codecs {
 		if sc, ok := codec.(*securecookie.SecureCookie); ok {
 			sc.MaxAge(age)
@@ -144,7 +144,7 @@ func (st *Store) MaxAge(age int) {
 	}
 }
 
-func (st *Store) MaxLength(l int) {
+func (st *BoltStore) MaxLength(l int) {
 	for _, c := range st.Codecs {
 		if codec, ok := c.(*securecookie.SecureCookie); ok {
 			codec.MaxLength(l)
@@ -152,11 +152,11 @@ func (st *Store) MaxLength(l int) {
 	}
 }
 
-func (st *Store) Cleanup() {
+func (st *BoltStore) Cleanup() {
 	st.store.DeleteMatching(&Session{}, bolthold.Where("ExpiresAt").Le(time.Now()))
 }
 
-func (st *Store) PeriodicCleanup(interval time.Duration, quit <-chan struct{}) {
+func (st *BoltStore) PeriodicCleanup(interval time.Duration, quit <-chan struct{}) {
 	t := time.NewTicker(interval)
 	defer t.Stop()
 	for {
